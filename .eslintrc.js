cbe891084b494054a5cd3693e3ab026329a74fdb
Austin Gil <austin@stegosource.com>
2020-08-03 19:25:19 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,48 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
+    // extras
+    jest: true
   },
-  extends: ["plugin:prettier/recommended"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  plugins: [
+    "prettier"
+    // "jest", "jsdoc", "markdown", "vue"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:jsdoc/recommended",
+    // "plugin:vue/recommended"
+    // "plugin:vuejs-accessibility/recommended",
+    // "plugin:jest/recommended"
+  ],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-  },
-  parser: "babel-eslint",
-}
+    indent: ["error", 2],
+    quotes: [
+      "error",
+      "single",
+      { avoidEscape: true, allowTemplateLiterals: true }
+    ],
+    semi: ["error", "always"],
+    "comma-dangle": ["error", "only-multiline"],
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+
+    "prettier/prettier": ["error", { "singleQuote": true }],
+
+    // "jsdoc/check-tag-names": [
+    //   "error",
+    //   {
+    //     definedTags: ["model", "slot"]
+    //   }
+    // ],
+
+    "vue/require-prop-types": ["error"]
+  }
+};
