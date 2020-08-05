aa3e59f45c2f49d07c673d32e52205f323c63eff
Austin Gil <austin@stegosource.com>
2020-08-05 15:58:49 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,84 +5,50 @@ module.exports = {
     node: true,
     es6: true,
 
-    jest: true,
+    jest: true
   },
   parserOptions: {
     parser: 'babel-eslint',
+    // "ecmaVersion": 6,
+    sourceType: 'module'
   },
   plugins: [
-    "jsdoc",
+    'jsdoc',
     // "jest",
-    // 'vue',
+    // 'vue'
   ],
   extends: [
     'eslint:recommended',
-    'plugin:prettier/recommended',
+    "plugin:prettier/recommended",
     'plugin:jsdoc/recommended',
-    // 'plugin:vue/recommended'
-    // "plugin:vuejs-accessibility/recommended",
     // "plugin:jest/recommended"
+    // 'plugin:vue/recommended',
+    // "plugin:vuejs-accessibility/recommended",
   ],
 
   rules: {
-    
-    'array-bracket-newline': ['error', { multiline: true }],
-    'array-bracket-spacing': ['error', 'never'],
-    'array-element-newline': ['error', 'consistent'],
-    'block-spacing': ['error'],
-    'brace-style': 'error',
-    'comma-spacing': [
-      'error', {
-        'before': false,
-        'after': true
-      }
-    ],
     'comma-dangle': ['error', 'only-multiline'],
-    'computed-property-spacing': ['error', 'never'],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'max-len': [
-      'error', {
-        'code': 80,
-        'ignoreComments': true 
-      }
-    ],
-    'no-mixed-spaces-and-tabs': 'error',
-    'no-multi-spaces': ['error'],
-    'no-multiple-empty-lines': ['error', { max: 1 }],
-    'no-whitespace-before-property': 'error',
-    'object-curly-newline': [
-      'error', {
-        'multiline': true,
-        'consistent': true
-      }
-    ],
-    'object-curly-spacing': ['error', 'always'],
-    'object-property-newline': ['error'],
-    quotes: [
-      'error', 'single', {
-        avoidEscape: true,
-        allowTemplateLiterals: true
-      }
-    ],
-    semi: ['error', 'always'],
-    'semi-spacing': ['error'],
-    'space-before-blocks': ['error'],
-    'space-in-parens': ['error', 'never'],
-    'space-infix-ops': 'error',
-    'space-unary-ops': 'error',
-    'spaced-comment': ['error'],
-    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-console': 'off',
 
+    "prettier/prettier": ['error', {
+      singleQuote: true,
+      trailingComma: 'es5'
+    }],
+
+    'jsdoc/require-jsdoc': 'off',
     'jsdoc/require-param-description': 'off',
     'jsdoc/require-returns-description': 'off',
-    // "jsdoc/check-tag-names": [
-    //   "error",
-    //   {
-    //     definedTags: ["model", "slot"]
-    //   }
-    // ],
 
     // 'vue/require-prop-types': ['error']
-  },
+    // "vue/max-attributes-per-line": 'off',
+    // 'vue/custom-event-name-casing': 'off',
+    // 'vue/html-self-closing': [
+    //   'error',
+    //   {
+    //     html: {
+    //       void: 'any'
+    //     }
+    //   }
+    ],
+  }
 };
